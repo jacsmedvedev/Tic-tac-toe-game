479c8a1b32b071946c5ab0e09ca1b120ea326140
Jacob <Jacob@DESKTOP-II969AC>
2017-10-09 23:24:05 +0300

    Commit_4
    - new class TicTacToeGameComboChecker for checking win combinations

--- src/console_version/TicTacToeGame.java
+++ src/console_version/TicTacToeGame.java
@@ -32,16 +32,7 @@ public class TicTacToeGame {
 	private static boolean ZERO;
 	private static boolean SOMEBODYWIN = false;
 	
-	private static int[] horizont_1 = {11,12,13};
-	private static int[] horizont_2 = {21,22,23};
-	private static int[] horizont_3 = {31,32,33};
 	
-	private static int[] vertical_1 = {11,21,31};
-	private static int[] vertical_2 = {12,22,32};
-	private static int[] vertical_3 = {13,23,33};
-	
-	private static int[] diagonal_1 = {11,22,33};
-	private static int[] diagonal_2 = {13,22,13};
 	
 	private static List<Integer> stepsEX = new ArrayList<Integer>();
 	private static List<Integer> stepsZERO = new ArrayList<Integer>();
