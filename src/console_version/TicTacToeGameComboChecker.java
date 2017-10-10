201672369ee878c8857074b10a8cd13209d05d0f
Jacob <Jacob@DESKTOP-II969AC>
2017-10-10 22:46:04 +0300

    Commit_5
    - class TicTacToeGameComboChecker is modified

--- src/console_version/TicTacToeGameComboChecker.java
+++ src/console_version/TicTacToeGameComboChecker.java
@@ -1,5 +1,11 @@
 package console_version;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class TicTacToeGameComboChecker {
 	
 	private static int[] horizont_1 = {11,12,13};
@@ -13,5 +19,38 @@ public class TicTacToeGameComboChecker {
 	private static int[] diagonal_1 = {11,22,33};
 	private static int[] diagonal_2 = {13,22,13};
 	
+	private static int[][] full =  {horizont_1,
+									horizont_2,
+									horizont_3,
+									vertical_1,
+									vertical_2,
+									vertical_3,
+									diagonal_1,
+									diagonal_2};
+	
+	
+	
+	
+	private static List<Integer> stepsEX = new ArrayList<Integer>();
+	private static List<Integer> stepsZERO = new ArrayList<Integer>();
+	
+	public static void main(String[] args) {
+		stepsEX.add(11);
+		stepsEX.add(22);
+		
+		System.out.println(check(33));
+	}
 
+	public static boolean check(int newMove) {
+		stepsEX.add(newMove);
+		boolean result = false;
+		for (int i = 0; i < full.length; i++) {
+			Set<Integer> mySet = Arrays.stream(full[i]).boxed().collect(Collectors.toSet());
+			result = stepsEX.containsAll(mySet);
+			if(result) {
+				break;
+			}
+		}
+		return result;
+	}
 }
